bf2fa57d28bf3aa4d5fbe42e2f8cf18334d01e87
virendra1984 <virendrams2014@gmail.com>
2020-05-05 11:42:40 +0530

    first commit

---
+++ src/main/java/com/springjdbcsecurity4/springjdbcsecurity4/SpringJdbcSecurity4Application.java
@@ -0,0 +1,13 @@
+package com.springjdbcsecurity4.springjdbcsecurity4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcSecurity4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcSecurity4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springjdbcsecurity4/springjdbcsecurity4/SpringJdbcSecurity4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjdbcsecurity4.springjdbcsecurity4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcSecurity4ApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
